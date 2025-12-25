b343bed38b918d179f7122545c133ae2abfbc408
pravindesai0790 <pravindesai0790@gmail.com>
2025-12-25 16:56:35 +0530

    Created QuestionService

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,6 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+#pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 6001)
-    .WithDataVolume("keycloak-data");
+    .WithDataVolume("keycloak-data")
+    .WithoutHttpsCertificate();
+#pragma warning restore ASPIRECERTIFICATES001
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak) // add reference to Keycloak service so thet questionservice can know how to locate the Keycloak service.
+    .WaitFor(keycloak); // wait to start keycloak service to start before starting questionservice 
 
 builder.Build().Run();
