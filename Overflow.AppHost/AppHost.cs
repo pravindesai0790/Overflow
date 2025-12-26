f2a31b101546540231f8e8f0514bd7029f67427d
pravindesai0790 <pravindesai0790@gmail.com>
2025-12-26 11:40:24 +0530

    Add and configured postgresql in apphost and question service

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -6,8 +6,16 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithoutHttpsCertificate();
 #pragma warning restore ASPIRECERTIFICATES001
 
+var postgres = builder.AddPostgres("postgres", port: 5432) // it will create postgres service in container
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb"); // it will create postgres DB as questionDB
+
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(keycloak) // add reference to Keycloak service so thet questionservice can know how to locate the Keycloak service.
-    .WaitFor(keycloak); // wait to start keycloak service to start before starting questionservice 
+    .WithReference(questionDb)
+    .WaitFor(keycloak) // wait to start keycloak service to start before starting questionservice 
+    .WaitFor(questionDb);
 
 builder.Build().Run();
