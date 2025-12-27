b2f1750937d005564f6f40059a573928458d591a
pravindesai0790 <pravindesai0790@gmail.com>
2025-12-27 09:57:37 +0530

    Added Typesense to the Aspire project and created SearchService project

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -6,16 +6,36 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithoutHttpsCertificate();
 #pragma warning restore ASPIRECERTIFICATES001
 
-var postgres = builder.AddPostgres("postgres", port: 5432) // it will create postgres service in container
+// it will create postgres service and PgAdmin in docker container with help of Aspire Host postgres integration
+var postgres = builder.AddPostgres("postgres", port: 5432) 
     .WithDataVolume("postgres-data")
     .WithPgAdmin();
 
-var questionDb = postgres.AddDatabase("questionDb"); // it will create postgres DB as questionDB
+// it will create postgres DB as questionDB 
+var questionDb = postgres.AddDatabase("questionDb"); 
 
+// question service configuration with keycloak service and postgres DB 
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(keycloak) // add reference to Keycloak service so thet questionservice can know how to locate the Keycloak service.
-    .WithReference(questionDb)
+    // add reference to postgres container so thet questionservice can know how to locate the DB and resources.
+    // it also manages connection string.
+    .WithReference(questionDb) 
     .WaitFor(keycloak) // wait to start keycloak service to start before starting questionservice 
     .WaitFor(questionDb);
+
+// It will create docker container for typesense with typesense image version 29.0 and add provide configuration with port number
+// This is the way how we can create resources which doesn't have Aspire host integration
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", "xyz", "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithEndpoint(8108, 8108, name: "typesense");
+
+// it will get the typesense container refence to add later it to our project.
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
+// search service configuration with typesense
+var searchService = builder.AddProject<Projects.SearchService>("search-svc") 
+    .WithReference(typesenseContainer)
+    .WaitFor(typesense);
 
 builder.Build().Run();
