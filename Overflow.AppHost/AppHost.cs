5efa3e38f3041c3b8e2a1ec65ba9b021a3f1519b
pravindesai0790 <pravindesai0790@gmail.com>
2025-12-29 13:36:28 +0530

    Created docker compose file using Aspire CLI and Aspire Docker nuget pkg

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,4 +1,9 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var compose = builder.AddDockerComposeEnvironment("production")
+    .WithDashboard(dashboard => dashboard.WithHostPort(8080));
 
 #pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 6001)
@@ -30,13 +35,19 @@ var questionService = builder.AddProject<Projects.QuestionService>("question-svc
     .WaitFor(rabbitmq);
 
 // It will get the secret stored using "dotnet user-secrets" in AppHost
-var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+//var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+
+var typesenseApiKey = builder.Environment.IsDevelopment()
+    ? builder.Configuration["Parameters:typesense-api-key"] // it will get it from user secrets directly
+      ?? throw new InvalidOperationException("Could not get typesense api key")
+    : "${TYPESENSE_API_KEY}"; // if we're running in docker
 
 // It will create docker container for typesense with typesense image version 29.0 and add provide configuration with port number
 // This is the way how we can create resources which doesn't have Aspire host integration
 var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
     .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
     .WithVolume("typesense-data", "/data")
+    .WithEnvironment("TYPESENSE_API_KEY", typesenseApiKey) // this will make available inside docker compose file for the typesense service
     .WithHttpEndpoint(8108, 8108, name: "typesense");
 
 // it will get the typesense container refence to add later it to our project.
