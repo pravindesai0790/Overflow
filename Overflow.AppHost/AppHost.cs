ca875d3225427485b68b148594743e36209230f9
pravindesai0790 <pravindesai0790@gmail.com>
2025-12-29 21:27:59 +0530

    Worked on keycloak realm export and import in docker container

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -8,6 +8,11 @@ var compose = builder.AddDockerComposeEnvironment("production")
 #pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak-data")
+    /*cmd to export keycloak realm json file
+     *   -> docker run --rm -v keycloak-data:/opt/keycloak/data -v ${PWD}/realms:/opt/keycloak/export quay.io/keycloak/keycloak:26.4 export --realm overflow --dir /opt/keycloak/export --users realm_file
+     */
+    .WithRealmImport("../infra/realms") // to import exported realms 
+
     /*
      * WARNING: With HTTPS not enabled, `proxy-headers` unset, and `hostname-strict=false`, the server is running in an insecure context.
      * Secure contexts are required for full functionality, including cross-origin cookies. Also, if you are using a proxy,
@@ -15,10 +20,10 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
      * Make sure `proxy-headers` are configured properly. Key material not provided to set up HTTPS. Please configure your keys/certificates,
      * or if HTTPS access is not needed see the `http-enabled` option. If you meant to start the server in development mode, see the `start-dev` command.
      */
-    .WithEnvironment("KC_HTTP_ENABLED", "true")  // Docker publishing error for https 
+    .WithEnvironment("KC_HTTP_ENABLED", "true") // Docker publishing error for https 
     .WithEnvironment("KC_HOSTNAME_STRICT", "false")
     .WithEndpoint(6001, 8080, "keycloak", isExternal: true) // to access keycloak management externally in browser
-    .WithoutHttpsCertificate(); // to remove unhealthy status from Aspire Host 
+    .WithoutHttpsCertificate(); // to remove unhealthy status from Aspire Host  
 #pragma warning restore ASPIRECERTIFICATES001
 
 // it will create postgres service and PgAdmin in docker container with help of Aspire Host postgres integration
