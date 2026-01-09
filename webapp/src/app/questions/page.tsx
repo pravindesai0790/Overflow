2177fc9e6861686758e38179913a7471a835cfe9
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-09 19:54:38 +0530

    Created Question page and types and displaying server fetched data init

---
+++ webapp/src/app/questions/page.tsx
@@ -0,0 +1,15 @@
+﻿import {getQuestions} from "@/lib/actions/question-actions";
+
+export default async function QuestionsPage() {
+    const questions = await getQuestions();
+    
+    return (
+        <div>
+            <ul>
+                {questions.map(question => (
+                    <li key={question.id}>{question.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
---
+++ webapp/src/lib/actions/question-actions.ts
@@ -0,0 +1,13 @@
+﻿'use server'
+
+import {Question} from "@/lib/types";
+
+export async function getQuestions(tags?: string): Promise<Question[]> {
+    let url = 'http://localhost:8001/questions';
+    if (tags) url += `?tags=${tags}`;
+    const response = await fetch(url);
+    
+    if(!response.ok) throw new Error('Failed to get data');
+    
+    return response.json();
+}
