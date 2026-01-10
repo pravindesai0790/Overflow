cd858d3aa0ddd9df4574062526a4f3fdacb8d7da
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-10 19:58:50 +0530

    Added a custome not found page

---
+++ webapp/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+﻿import {Button} from "@heroui/button";
+import Link from "next/dist/client/link";
+
+export default function NotFound() {
+    return (
+        <div className='flex h-full items-center justify-center'>
+            <div className='text-center space-y-6'>
+                <h1 className='text-5xl font-bold'>404 - Page Not Found</h1>
+                <p className='text-lg text-base-content/80'>
+                    Sorry, the page you are looking for doesn&#39;t exist.
+                </p>
+                <Button as={Link} href='/' color='primary'>Go Home</Button>
+            </div>
+        </div>
+    );
+}
