c5c969acde544d3945d85e65755a8bb3f16bcea1
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-12 20:35:06 +0530

    Added NextJS 'Data Caching' to getTags API call

--- webapp/src/lib/actions/tag-actions.ts
+++ webapp/src/lib/actions/tag-actions.ts
@@ -4,5 +4,5 @@ import {fetchClient} from "@/lib/fetchClient";
 import {Tag} from "@/lib/types";
 
 export async function getTags() {
-    return fetchClient<Tag[]>('/tags', 'GET');
+    return fetchClient<Tag[]>('/tags', 'GET', {cache: 'force-cache', next:{revalidate: 3600}});
 }
