96856327b1a052eba5e199430c7023d0537be418
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-12 08:13:50 +0530

    Added quuestion and answer footer

--- webapp/src/app/questions/[id]/AnswerContent.tsx
+++ webapp/src/app/questions/[id]/AnswerContent.tsx
@@ -1,5 +1,6 @@
 ﻿import {Answer} from "@/lib/types";
 import VotingButtons from "@/app/questions/[id]/VotingButtons";
+import AnswerFooter from "@/app/questions/[id]/AnswerFooter";
 
 type Props = {
     answer: Answer;
@@ -9,10 +10,13 @@ export default function AnswerContent({answer}: Props) {
     return (
         <div className='flex border-b pb-3 px-6'>
             <VotingButtons accepted={answer.accepted}/>
-            <div
-                className='flex-1 mt-4 ml-6 prose max-w-none dark:prose-invert'
-                dangerouslySetInnerHTML={{__html: answer.content}}
-            />
+            <div className='flex flex-col'>
+                <div
+                    className='flex-1 mt-4 ml-6 prose max-w-none dark:prose-invert'
+                    dangerouslySetInnerHTML={{__html: answer.content}}
+                />
+                <AnswerFooter answer={answer} />
+            </div>
         </div>
     );
 }
