cb71792a434dc07583da115b8a20be80de13e9e2
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-13 19:30:46 +0530

    Added dropdown UI as usermenu for loggin user

--- webapp/src/lib/actions/auth-actions.ts
+++ webapp/src/lib/actions/auth-actions.ts
@@ -1,7 +1,21 @@
 ﻿'use server';
 
 import {fetchClient} from "@/lib/fetchClient";
+import {auth} from "@/auth";
 
 export async function testAuth() {
     return fetchClient<string>(`/test/auth`, 'GET');
+}
+
+export async function getCurrentUser() {
+    try {
+        const session = await auth();
+        
+        if(!session) return null;
+        
+        return session.user;
+    } catch(err: unknown) {
+        console.log(err);
+        return null;
+    }
 }
