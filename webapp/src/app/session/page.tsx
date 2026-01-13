8d92a12049fe8c14d032d999b38d715c85de4252
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-13 20:23:42 +0530

    Storing the access token within session when user login first time using callbacks used Module Augmentation to extend these types to add 'accessToken' property in Session and JWT across Auth.js

--- webapp/src/app/session/page.tsx
+++ webapp/src/app/session/page.tsx
@@ -1,11 +1,30 @@
 ﻿import ErrorButtons from "@/app/session/ErrorButtons";
 import AuthTestButton from "@/app/session/AuthTestButton";
+import {auth} from "@/auth";
+import {Snippet} from "@heroui/snippet";
 
-export default function Page() {
+export default async function Page() {
+    const session = await auth();
+    
     return (
-        <div className='flex items-center justify-center gap-3 mt-6'>
-            <ErrorButtons />
-            <AuthTestButton />
+        <div className='px-6'>
+            <div className='text-center'>
+                <h3 className='text-3xl'>Session dashboard</h3>
+            </div>
+            <Snippet
+                symbol=''
+                color='primary'
+                classNames={{
+                    base: 'w-full mt-4',
+                    pre: 'text-wrap whitespace-pre-wrap break-all'
+                }}
+            >
+                {JSON.stringify(session, null, 2)}
+            </Snippet>
+            <div className='flex items-center justify-center gap-3 mt-6'>
+                <ErrorButtons />
+                <AuthTestButton />
+            </div>
         </div>
     );
 }
--- webapp/src/auth.ts
+++ webapp/src/auth.ts
@@ -3,4 +3,18 @@ import Keycloak from "next-auth/providers/keycloak"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Keycloak],
+    callbacks: {
+        async jwt({token, account}) {
+            if(account && account.access_token) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({session, token}) {
+            if(token) {
+                session.accessToken = token.accessToken;
+            }
+            return session;
+        }
+    }
 })
