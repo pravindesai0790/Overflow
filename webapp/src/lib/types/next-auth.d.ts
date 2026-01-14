9a33ddc43288b77d7c488e90367aae7028dc4139
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-14 19:50:20 +0530

    Added code to get new token from the refresh token if it expired

--- webapp/src/lib/types/next-auth.d.ts
+++ webapp/src/lib/types/next-auth.d.ts
@@ -12,5 +12,8 @@ declare module 'next-auth' {
 declare module 'next-auth/jwt' {
     interface JWT {
         accessToken: string;
+        refreshToken: string;
+        accessTokenExpires: number;
+        error?: string;
     }
 }
