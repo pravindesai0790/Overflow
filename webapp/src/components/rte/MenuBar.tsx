23422b35983f16776c6a387fc167152108d00d71
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-15 20:16:57 +0530

    Added Menubar for richtexteditor

---
+++ webapp/src/components/rte/MenuBar.tsx
@@ -0,0 +1,67 @@
+﻿import {Editor} from "@tiptap/core";
+import {useEditorState} from "@tiptap/react";
+import {BoldIcon, CodeBracketIcon, ItalicIcon, StrikethroughIcon} from "@heroicons/react/20/solid";
+import {Button} from "@heroui/button";
+
+type Props = {
+    editor: Editor | null;
+}
+
+export default function MenuBar({ editor }: Props) {
+    const editorState = useEditorState({
+        editor,
+        selector: ({editor}) => {
+            if (!editor) return null;
+            
+            return {
+                isBold: editor.isActive('bold'),
+                isItalic: editor.isActive('italic'),
+                isCodeBlock: editor.isActive('code-block'),
+                isStrike: editor.isActive('strike'),
+            }
+        }
+    })
+    
+    if(!editor || !editorState) return null;
+    
+    const options = [
+        {
+            icon: <BoldIcon className='w-5 h-5' />,
+            onClick: () => editor.chain().focus().toggleBold().run(),
+            pressed: editorState.isBold
+        },
+        {
+            icon: <ItalicIcon className='w-5 h-5' />,
+            onClick: () => editor.chain().focus().toggleItalic().run(),
+            pressed: editorState.isItalic
+        },
+        {
+            icon: <StrikethroughIcon className='w-5 h-5' />,
+            onClick: () => editor.chain().focus().toggleStrike().run(),
+            pressed: editorState.isStrike
+        },
+        {
+            icon: <CodeBracketIcon className='w-5 h-5' />,
+            onClick: () => editor.chain().focus().toggleCodeBlock().run(),
+            pressed: editorState.isCodeBlock
+        }
+    ]
+    
+    return (
+        <div className='rounded-md space-x-1 pb-1 z-50'>
+            {options.map((option, index) => (
+                <Button
+                    key={index}
+                    type='button'
+                    radius='sm'
+                    size='sm'
+                    isIconOnly
+                    color={option.pressed ? 'primary' : 'default'}
+                    onPress={option.onClick}
+                >
+                    {option.icon}
+                </Button>
+            ))}
+        </div>
+    );
+}
--- webapp/src/components/rte/RichTextEditor.tsx
+++ webapp/src/components/rte/RichTextEditor.tsx
@@ -1,5 +1,6 @@
 ﻿import {EditorContent, useEditor} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import MenuBar from "@/components/rte/MenuBar";
 
 export default function RichTextEditor() {
     const editor = useEditor({
@@ -7,13 +8,17 @@ export default function RichTextEditor() {
         content: '<p>Hello overflow</p>',
         editorProps: {
             attributes: {
-                class: 'w-full p-3 bg-default-100 rounded-xl min-h-60 prose dark:prose-invert max-w-none'
+                class: 'w-full p-3 bg-default-100 rounded-xl min-h-60 prose dark:prose-invert max-w-none ' +
+                    'dark:prose-pre:bg-primary-100'
             }
         },
         immediatelyRender: false
     });
     
     return (
-        <EditorContent editor={editor} />
+        <div>
+            <MenuBar editor={editor} />
+            <EditorContent editor={editor} />
+        </div>
     );
 }
