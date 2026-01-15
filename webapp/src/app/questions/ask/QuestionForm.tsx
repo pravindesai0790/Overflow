4df6368616d926bf7d078475ca9887c9c542e734
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-15 12:17:26 +0530

    Added react hook form with zod(schema validation) in QuestionForm

--- webapp/src/app/questions/ask/QuestionForm.tsx
+++ webapp/src/app/questions/ask/QuestionForm.tsx
@@ -5,15 +5,23 @@ import {Form} from "@heroui/form";
 import {Input, Textarea} from "@heroui/input";
 import {Select, SelectItem} from "@heroui/select";
 import {Button} from "@heroui/button";
+import {useForm} from "react-hook-form";
+import {QuestionSchema} from "@/lib/schemas/questionSchema";
 
 export default function QuestionForm() {
     const tags = useTagStore(state => state.tags);
+    const {register, handleSubmit, formState: {isSubmitting, isValid, errors}} = useForm<QuestionSchema>();
+    
+    const onSubmit = (data: QuestionSchema) => {
+        console.log(isSubmitting, isValid, data);
+    }
     
     return (
-        <Form className='flex flex-col gap-3 p-6 shadow-xl bg-white dark:bg-black'>
+        <Form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3 p-6 shadow-xl bg-white dark:bg-black'>
             <div className='flex flex-col gap-3 w-full'>
                 <h3 className='text-2xl font-semibold'>Title</h3>
-                <Input 
+                <Input
+                    {...register('title')}
                     type='text'
                     className='w-full'
                     label='Be specific and imagine you are asking a question to another person'
@@ -24,6 +32,7 @@ export default function QuestionForm() {
             <div className='flex flex-col gap-3 w-full'>
                 <h3 className='text-2xl font-semibold'>Body</h3>
                 <Textarea
+                    {...register('content')}
                     className='w-full'
                     label='Include all the information someone would need to answer your question'
                     labelPlacement='outside-top'
@@ -34,6 +43,7 @@ export default function QuestionForm() {
                 <h3 className='text-2xl font-semibold'>Tags</h3>
                 <p className='text-sm'>Add up to 5 tags to describe what your question is about</p>
                 <Select
+                    {...register('tags')}
                     className='w-full'
                     label='Select 1-5 tags'
                     selectionMode='multiple'
@@ -44,7 +54,13 @@ export default function QuestionForm() {
                     {(tag) => <SelectItem key={tag.id}>{tag.name}</SelectItem> }
                 </Select>
             </div>
-            <Button color='primary' className='w-fit'>Post your question</Button>
+            <Button 
+                type='submit'
+                color='primary' 
+                className='w-fit'
+            >
+                Post your question
+            </Button>
         </Form>
     );
 }
---
+++ webapp/src/lib/schemas/questionSchema.ts
@@ -0,0 +1,16 @@
+﻿import {z} from "zod";
+
+const required = (name: string) => z.string().trim().min(1, 
+    {message: `${name} is required`});
+
+const questionSchema = z.object({
+    title: required("Title"),
+    content: required("Content").min(10, 
+        {message: 'Content should be at least 10 characters'}),
+    tags: z.array(z.string())
+        .min(1, {message: 'Select at least 1 tag'})
+        .max(5, {message: 'No more than 5 tags can be selected'}),
+});
+
+export type QuestionSchema = z.infer<typeof questionSchema>
+
