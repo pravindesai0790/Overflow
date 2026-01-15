b3d5ac1f20cdeee04d8ed991d1a6a6bdcb88b890
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-15 11:54:46 +0530

    Added Ask question form component

---
+++ webapp/src/app/questions/ask/QuestionForm.tsx
@@ -0,0 +1,50 @@
+﻿'use client';
+
+import {useTagStore} from "@/lib/useTagStore";
+import {Form} from "@heroui/form";
+import {Input, Textarea} from "@heroui/input";
+import {Select, SelectItem} from "@heroui/select";
+import {Button} from "@heroui/button";
+
+export default function QuestionForm() {
+    const tags = useTagStore(state => state.tags);
+    
+    return (
+        <Form className='flex flex-col gap-3 p-6 shadow-xl bg-white dark:bg-black'>
+            <div className='flex flex-col gap-3 w-full'>
+                <h3 className='text-2xl font-semibold'>Title</h3>
+                <Input 
+                    type='text'
+                    className='w-full'
+                    label='Be specific and imagine you are asking a question to another person'
+                    labelPlacement='outside-top'
+                    placeholder='e.g how would you truncate text in tailwinf'
+                />
+            </div>
+            <div className='flex flex-col gap-3 w-full'>
+                <h3 className='text-2xl font-semibold'>Body</h3>
+                <Textarea
+                    className='w-full'
+                    label='Include all the information someone would need to answer your question'
+                    labelPlacement='outside-top'
+                    minRows={12}
+                />
+            </div>
+            <div className='flex flex-col gap-3 w-full'>
+                <h3 className='text-2xl font-semibold'>Tags</h3>
+                <p className='text-sm'>Add up to 5 tags to describe what your question is about</p>
+                <Select
+                    className='w-full'
+                    label='Select 1-5 tags'
+                    selectionMode='multiple'
+                    isClearable
+                    disallowEmptySelection
+                    items={tags}
+                >
+                    {(tag) => <SelectItem key={tag.id}>{tag.name}</SelectItem> }
+                </Select>
+            </div>
+            <Button color='primary' className='w-fit'>Post your question</Button>
+        </Form>
+    );
+}
---
+++ webapp/src/app/questions/ask/page.tsx
@@ -0,0 +1,10 @@
+﻿import QuestionForm from "@/app/questions/ask/QuestionForm";
+
+export default function Page() {
+    return (
+        <div className='px-6'>
+            <h3 className='text-3xl font-semibold pb-3'>Ask a public question</h3>
+            <QuestionForm />
+        </div>
+    );
+}
