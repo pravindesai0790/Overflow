8d365b1b6cd7f61b0b26e78d041574ec7ca18bfb
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-24 18:13:47 +0530

    Added the models and the DB to the VoteService

--- VoteService/Program.cs
+++ VoteService/Program.cs
@@ -1,4 +1,6 @@
 using Common;
+using Microsoft.EntityFrameworkCore;
+using VoteService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,7 @@ await builder.UseWolverineWithRabbitMqAsync(opts =>
 {
     opts.ApplicationAssembly = typeof(Program).Assembly;
 });
+builder.AddNpgsqlDbContext<VoteDbContext>("voteDb");
 
 var app = builder.Build();
 
@@ -17,6 +20,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+}
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider; // it provides access to all services available inside our app including DBContext
+try
+{
+    var context = services.GetRequiredService<VoteDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred while migrating or seeding the DB.");
 }
 
 app.Run();
